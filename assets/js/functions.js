eac256b2e9486eea87d0c11cbfb3cab3bbfd7177
Jarand Nikolai Jansen <JarandJansen182@gmail.com>
2017-01-05 20:21:43 +0100

    Spawn animations, nav links fixed. About-hero hover effect.
    Spawn animations when entering site and on scroll.
    
    When clicking the links in the nav. the sidebar closes and there is a
    scrolling animation.
    
    Added hidden content on the three about heroes - the content appears on
    hover.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,46 @@
 $( document ).ready(function() {
-  // Get started!
+
+  $("ul > li > a").click(function(e) {
+        // Prevent a page reload when a link is pressed
+      e.preventDefault();
+        // Call the scroll function
+      goToByScroll(this.id);
+  });
+
+//This sequence of code shows/spawns divs when they are in the viewport
+  $(window).on('scroll');
+
+  var status = [];
+
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+
+      /* Check the location of each desired element */
+      $('.hideme').each( function(i, el){
+
+          if (status[i] !== 'showing') { // Makes sure that we haven't already done this item
+
+              var bottom_of_object = $(this).position().top + $(this).outerHeight() -100;
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+              /* If the object is completely visible in the window, fade it in*/
+              if( bottom_of_window > (bottom_of_object) ){
+
+                  $(this).animate({'opacity':'1'},500);
+                  status[i] = 'showing';
+
+                  if ($('.hideme').index($(this)) + 1 === $('.hideme').length) {
+                      // We've shown them all, stop listening for scroll events!!!
+                      $(window).off('scroll');
+                  }
+
+              }
+
+          }
+
+      });
+
+  });
 
 
 
@@ -16,3 +57,16 @@ $( document ).ready(function() {
 
   });
 });
+
+function goToByScroll(id){
+    // Remove "link" from the ID
+  id = id.replace("link", "");
+
+  $('.side-list').toggleClass('reveal');
+  $('.closenav').removeClass('show');
+  $('.hamburger').show();
+    // Scroll
+  $('html,body').animate({
+      scrollTop: $("."+id).offset().top},
+      'slow');
+}
